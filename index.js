7b3ba7a281a8454e5ec8f6297d6a7c9fd3193982
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-04-05 13:56:07 -0700

    mvp all tests passing

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
-const port = 5000;
+const port = 4000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
